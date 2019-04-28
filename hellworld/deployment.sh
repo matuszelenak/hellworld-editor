5554111cd5b226d9c980952cd900a1ce77095787
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2019-04-28 11:27:14 +0200

    Collect without prompt

--- hellworld/deployment.sh
+++ hellworld/deployment.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata initial
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 gunicorn --bind 0.0.0.0:$PORT hellworld.wsgi --env DJANGO_SETTINGS_MODULE=hellworld.settings.production --log-level DEBUG --access-logfile - --log-file -
